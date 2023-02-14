7f5f63d45def3c54ac6eee44de722d45400dc0b2
Nagesh <nagesh0468@gmail.com>
2023-02-14 15:04:17 +0530

    hello, this is realton project using tailwind css this partis installtion and setup tailwind css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="bg-red-400 text-sm " >
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
